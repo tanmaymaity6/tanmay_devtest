5774dc34e1921bf86ff2cb344d3117d46832c0d6
tanmaymaity <53228036+tanmaymaity6@users.noreply.github.com>
2025-08-12 18:42:26 -0500

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import { FaDownload } from 'react-icons/fa'
 import { HiArrowNarrowRight } from 'react-icons/hi'
 import { Link } from 'react-scroll'
 import { useTypewriter, Cursor } from 'react-simple-typewriter'
-import resume from '../assets/Tanmay_Maity_Resume.pdf'
+import resume from '../assets/arjunsingh.pdf'
 
 export const Header = () => {
   const [text] = useTypewriter({
@@ -21,13 +21,13 @@ export const Header = () => {
         </h2>
         <p className='text-[#8892b0] py-4  max-w-[700px] '>An Aspiring Web Developer. I learn new things and make good projects.</p>
         <div className='flex gap-5 items-center'>
-          <Link to="home-projects1" smooth={true} duration={500}>
+          <Link to="home-projects" smooth={true} duration={500}>
             <button className='group bg-[#87CEEB] border-2  border-[#87CEEB] text-slate-950 px-1 py-1 md:px-6 md:py-3 my-2 flex items-center duration-200'>
               View Projects
               <span className='ml-2 group-hover:rotate-90 duration-300'><HiArrowNarrowRight /></span>
             </button>
           </Link>
-          <a href={resume} download={"Tanmay_Maity_Resume.pdf"} className='bg-[#87CEEB] border-2  border-[#87CEEB] text-slate-950 px-1 py-1 md:px-6 md:py-3 md:my-2 flex items-center duration-200'>
+          <a href={resume} download={""} className='bg-[#87CEEB] border-2  border-[#87CEEB] text-slate-950 px-1 py-1 md:px-6 md:py-3 md:my-2 flex items-center duration-200'>
             Resume <FaDownload className='ml-1' />
 
           </a>
